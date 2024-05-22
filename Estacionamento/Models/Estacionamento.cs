cdfef3be9106e4afefb46b7ccf1060690b32e76b
Murilo57 <murilo.ti32@outlook.com>
2024-05-21 22:33:32 -0300

    inicio do desafio

---
+++ Estacionamento/Models/Estacionamento.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Estacionamento
+{
+    double valorPorHora = 10;
+    List<string> veiculos = new List<string>();
+
+    public Estacionamento(decimal valorPorHora, decimal tempoEstacionado)
+    {
+      this.valorPorHora     = valorPorHora;
+      this.tempoEstacionado = tempoEstacionado;
+    }
+
+    public void AdicionarVeiculos()
+    {        
+      Console.WriteLine("Digite a placa do veiculo que ira estacionar: ");
+      string Placas = Console.ReadLine();
+      veiculos.Add(Placas);
+    }
+
+    public void RemoverVeiculos()
+    {
+      Console.WriteLine("Digite a placa do veiculo que ira sair: ");
+      veiculoSaida = Console.ReadLine();
+      if (veiculos.Any(x => x.ToUpper() == veiculoSaida.ToUpper()))
+      {
+        double horasEstacionado = ReadLine("Quanto tempo estacionado?: ");
+      }
+    }    
+}
---
+++ Estacionamento/Program.cs
@@ -0,0 +1,39 @@
+﻿using Estacionamento.Models;
+
+namespace Estacionamento;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        Console.WriteLine("--Bem vindo ao MF Estacionamentos!---");
+
+        Console.WriteLine("Digite a ação:");
+        Console.WriteLine("1 - Cadastrar Veiculo");
+        Console.WriteLine("2 - Remover Veiculo");
+        Console.WriteLine("3 - Listar veículos");
+        Console.WriteLine("4 - Encerrar");
+
+        Estacionamento es = new.Estacionamento(2,3);
+
+        switch(Console.ReadLine())
+        {
+            case "1":
+                es.CadastrarVeiculo();
+                break;
+            case "2":
+                es.RemoverVeiculo();
+                break;
+
+        }
+
+        //Console.WriteLine("Quantas horas pretende ficar(Minimo 1 hora):");
+
+
+
+
+
+    }
+}
