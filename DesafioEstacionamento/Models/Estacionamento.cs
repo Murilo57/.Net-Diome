afcc6d04950049fd3d7da96296dcaf0a6625d05b
Murilo57 <murilo.ti32@outlook.com>
2024-05-23 13:33:45 -0300

    Atualizando desafio

---
+++ DesafioEstacionamento/Models/Estacionamento.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DesafioEstacionamento.Models
+{
+    public class Estacionamento
+    {
+        double valorPorHora = 10;
+        double tempoEstacionado = 0;
+        string Placa = "";
+        List<string> veiculos = new List<string>();
+
+        public Estacionamento(double valorPorHora, double tempoEstacionado, string Placa)
+        {
+            this.valorPorHora = valorPorHora;
+            this.tempoEstacionado = tempoEstacionado;
+            this.Placa = Placa;
+        }
+
+        public CadastrarVeiculos(novaPlaca Placa)
+        {
+            Console.WriteLine("Digite a placa:");
+            novaPlaca = Console.ReadLine();
+            if (novaPlaca <> "");
+            {
+                veiculos.Add(novaPlaca);
+                Console.WriteLine("Carro cadastrado com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("Erro ao cadastrar placa.");
+            }
+            
+            
+        }
+
+        public RemoverVeiculos()
+        {
+            Console.WriteLine("Digite a placa do veiculo que ira sair: ");
+            string veiculoSaida = Console.ReadLine();
+            if (veiculos.Any(x => x.ToUpper() == veiculoSaida.ToUpper()))
+            {
+                double horasEstacionado = ReadLine("Quanto tempo estacionado?: ");
+            }
+        }
+    }
+}
---
+++ DesafioEstacionamento/Program.cs
@@ -0,0 +1,40 @@
+﻿using DesafioEstacionamento.Models;
+
+
+
+Estacionamento es = new Estacionamento(2, 5);
+
+string opcao = string.Empty;
+bool exibirMeny = true;
+
+while (exibirMenu)
+{
+
+    Console.WriteLine("--Bem vindo ao MF Estacionamentos!---");
+    Console.WriteLine("Digite a ação:");
+    Console.WriteLine("1 - Cadastrar Veiculo");
+    Console.WriteLine("2 - Remover Veiculo");
+    Console.WriteLine("3 - Listar veículos");
+    Console.WriteLine("4 - Encerrar");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            es.CadastrarVeiculo();
+            break;
+        case "2":
+            es.RemoverVeiculo();
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+
+    }
+}
+
+//Console.WriteLine("Quantas horas pretende ficar(Minimo 1 hora):");
+
+
+
+
